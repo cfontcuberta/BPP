5f4a635fea5e0c30d4527f12663e415e52d6abee
cfontcuberta <64101455+cfontcuberta@users.noreply.github.com>
2022-07-07 16:26:55 +0200

    Add files via upload

--- matematicas.py
+++ matematicas.py
@@ -1,78 +1,118 @@
+"""Calcular algunas operaciones matematicas
+
+**Atributos**
+--------------
+        **ele1 :**
+              operador para el calculo de un solo numero 
+        **ele2 :** 
+              cuando se requieren mas de una variable
+
+**Metodos**
+-------------
+
+factorial
+         Calcula el factorial de un numero
+cuadrada
+        calcula la raiz cuadrada de un numero
+Hipo 
+        Calcula la hipotenusa
+area
+        Calcula el area de un circulo
+
+rectagunlo
+        Calcula el area de un rectangulo
+
+**Ejemplo**
+------------
+>>>>>factorial(5)
+>>>>>>cuadrada(3)
+>>>>>>hipo(5,3)
+>>>>>>>area(3)
+>>>>>>>>rectangulo(5,4)
+
 """
-Programa que calcula una serie de operaciones Matematicas
-
-    Factorial    
-    Raiz Cuadrada
-    Hipotenusa
-    El area de un circulo
-    El area de un cuadrado
 
 
-"""
-    
-"""_summary_
-funcion para calcular el factorial de un numero
+class matematicas:
+    """Construccion de la clase para calcular algunas 
+       operaciones matematicas
+    """
+    def __init__(self,ele1,ele2):
+        self.ele1 = ele1
+        self.ele2 = ele2
 
-Variable a pasar un numero y returna el factorial de ese numero
+    def factorial(self,ele):
+        """funcion que calcula el factorial de un numero
 
-"""
-def factorial(num): 
-    if num < 0: 
-        print("Factorial of negative num does not exist")
+    Args:
+        ele (self): argumento por defecto
 
-    elif num == 0: 
-        return 1
+    Returns:
+        fact: el factorial del numero
+    """
+        if self.ele1  < 0: 
+            print("Factorial de un numero negarivo no existe")
+        elif self.ele1 == 0: 
+            return 1
         
-    else: 
-        fact = 1
-        while(num > 1): 
-            fact *= num 
-            num -= 1
+        else: 
+            fact = 1
+        while(self.ele1 > 1): 
+            fact *= self.ele1
+            self.ele1 -= 1
         return fact 
 
 
-    """_summary_
-    Calculo de la Raiz Cuadrada
+    def cuadrada(self,ele):
+        """funcion que calcula la raiz cuadrada de un numero
+
+    Args:
+        ele : numero a calcular
+
+    Returns:
+        raiz: la raiz cuadrada del numero
     """
-def cuadrada(num):
-     raiz = num** 0.5
-     return raiz
+        raiz = self.ele1** 0.5
+        return raiz
 
-"""
-    _summary_
-    Calculo de la Hipotenusa 
 
-    Returns:
-        _type_: _description_
-"""
-def hipo(a,b):
-    h = (a*a) + (b*b)
-    return h
 
-"""_summary_
-   Calcular el area de un circulo
+    def hipo(self,ele):
+        """Funcion que calcula la hipotenusa
+
+    Args:
+        ele :  valores a calcular
 
     Returns:
-        _type_: _description_
- """
+        hipo: retorna la hipotenusa
+    """
+        h = (self.ele1**2) + (self.ele2**2)
+        return h
 
-def area(r):
-    pi = 3.141592
-    a = pi * (r*r)
-    return a
-"""_summary_
-funcion para calcular el factorial de un numero
 
- Variable a pasar un numero y returna el factorial de ese numero
+    def area(self,ele):
+        """funcion que calcula el area de un circulo
+       se toma PI = 3.141592
+
+    Args:
+        ele : radio del circulo
 
     Returns:
-        _type_: _description_
-"""
+        ele : el area del circulo
+    """
+        pi = 3.141592
+        a = pi * (self.ele1**2)
+        return a
 
-"""_summary_
-Funcion que calcula el area de un rectangulo
-"""
 
-def rectangulo(a,l):
-    a1 = a * l
-    return a1
+    def rectangulo(self,ele):
+        """Funcion que calcula el area de un rectagunlo
+
+    Args:
+        ele:   se pasa el ancho y largo
+
+    Returns:
+       area : devuelve el area del rectangulo
+    """
+        a1 = self.ele1 * self.ele2
+        return a1
