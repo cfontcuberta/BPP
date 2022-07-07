ecb6e9828b12d03cfa1e8f39caffd2b72d3437a7
bitaminadas <soyyo@gmail.com>
2022-07-07 23:39:31 +0200

    cfontcuberta_act4:

--- cfontcuberta_act4.py
+++ cfontcuberta_act4.py
@@ -7,7 +7,7 @@
 # Importacion de la libreria para depurar el codigo
 import pdb
 pdb.set_trace()
-
+# compresion de listas
 lista = [[2, 4, 1], [1,2,3,4,5,6,7,8], [100,250,43]]
 maxValues = [max(i) for i in lista]
 
