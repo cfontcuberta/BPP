84cacfd770b7483ff2123e382bc877475c09f9c4
cfontcuberta <64101455+cfontcuberta@users.noreply.github.com>
2022-07-07 16:33:27 +0200

    Add files via upload

---
+++ cfontcuberta_act4.py
@@ -0,0 +1,14 @@
+"""
+    Actividad relacionada con la lección 4:
+
+    Scrip que utilizando la compresion de listas determina el numero mayor de cada lista
+    
+"""
+# Importacion de la libreria para depurar el codigo
+import pdb
+pdb.set_trace()
+
+lista = [[2, 4, 1], [1,2,3,4,5,6,7,8], [100,250,43]]
+maxValues = [max(i) for i in lista]
+
+
